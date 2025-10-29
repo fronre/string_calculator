9511c31cbd1cbb8c36d9f8cc9122003f3ad39d95
fronre <islamhala35@gmail.com>
2025-10-29 20:29:27 +0100

    many test

--- Testes/Test_String_Calculator.py
+++ Testes/Test_String_Calculator.py
@@ -1,10 +1,19 @@
 from src.String_Calculator import String_Calculator
-def test_string_Calculator():
 
-  def test_empty_string_returns_0():
-        assert String_Calculator('') == 0
+def test_empty_string_returns_0():
+    assert String_Calculator('') == 0
 
-  def test_single_number_returns_same_value():
-      assert String_Calculator("1") == 1
+def test_single_number_returns_same_value():
+    assert String_Calculator("1") == 1
 
+def test_string_number2_returns_2():
+    assert String_Calculator("2") == 2
 
+def test_string_number3_returns_3():
+    assert String_Calculator("3") == 3
+
+def test_string_number4_returns_4():
+    assert String_Calculator("4") == 4
+
+def test_string_number5_returns_5():
+    assert String_Calculator("5") == 5
--- src/String_Calculator.py
+++ src/String_Calculator.py
@@ -3,4 +3,12 @@ def String_Calculator(string_numbers: str) -> int:
         return 0
     if string_numbers == "1":
         return 1
+    if string_numbers == "2":
+        return 2
+    if string_numbers == "3":
+        return 3
+    if string_numbers == "4":
+        return 4
+    if string_numbers == "5":
+        return 5
     return 0
