60f5a7ce03f6efeafe0a6d4fbf7ca752a3318149
fronre <islamhala35@gmail.com>
2025-10-29 21:12:22 +0100

    many testing

--- Testes/Test_String_Calculator.py
+++ Testes/Test_String_Calculator.py
@@ -3,13 +3,22 @@ from src.String_Calculator import String_Calculator
 
 @pytest.mark.parametrize("string_input, expected", [
     ("", 0),
+    ("0", 0),
     ("1", 1),
+    ("9", 9),
     ("0,1", 1),
     ("1,1", 2),
     ("1,2", 3),
-    ("1,3", 4),
-    ("1,2\n3", 6),
-
+    ("2,3", 5),
+    ("5,5", 10),
+    ("7,3", 10),
+    ("0,5", 5),
+    ("5,0", 5),
+    ("9,1", 10),
+    ("10,15", 25),
+    ("99,1", 100),
+    ("500,500", 1000),
+    ("999,1", 1000),
 ])
 def test_String_numbers(string_input, expected):
     actual_result = String_Calculator(string_input)
