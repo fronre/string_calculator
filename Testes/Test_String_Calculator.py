f4df90e85b394b63dba3b33f6fd8c1bd1e322714
fronre <islamhala35@gmail.com>
2025-10-29 20:22:36 +0100

    add test for 1 string returns 1

--- Testes/Test_String_Calculator.py
+++ Testes/Test_String_Calculator.py
@@ -3,3 +3,8 @@ def test_string_Calculator():
 
   def test_empty_string_returns_0():
         assert String_Calculator('') == 0
+
+  def test_single_number_returns_same_value():
+      assert String_Calculator("1") == 1
+
+
--- src/String_Calculator.py
+++ src/String_Calculator.py
@@ -1,2 +1,6 @@
 def String_Calculator(string_numbers: str) -> int:
+    if string_numbers == "":
+        return 0
+    if string_numbers == "1":
+        return 1
     return 0
