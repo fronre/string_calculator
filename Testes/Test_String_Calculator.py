bfbd360090e214772fb498638a3990fbc8d30b10
fronre <islamhala35@gmail.com>
2025-10-29 20:46:10 +0100

    refactoring

--- Testes/Test_String_Calculator.py
+++ Testes/Test_String_Calculator.py
@@ -1,19 +1,13 @@
+import pytest
 from src.String_Calculator import String_Calculator
 
-def test_empty_string_returns_0():
-    assert String_Calculator('') == 0
-
-def test_single_number_returns_same_value():
-    assert String_Calculator("1") == 1
-
-def test_string_number2_returns_2():
-    assert String_Calculator("2") == 2
-
-def test_string_number3_returns_3():
-    assert String_Calculator("3") == 3
-
-def test_string_number4_returns_4():
-    assert String_Calculator("4") == 4
-
-def test_string_number5_returns_5():
-    assert String_Calculator("5") == 5
+@pytest.mark.parametrize("input_str, expected", [
+    ("", 0),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+])
+def test_String_numbers(string_input, expected):
+    assert String_Calculator(string_input) == expected
--- src/String_Calculator.py
+++ src/String_Calculator.py
@@ -1,14 +1,4 @@
 def String_Calculator(string_numbers: str) -> int:
     if string_numbers == "":
         return 0
-    if string_numbers == "1":
-        return 1
-    if string_numbers == "2":
-        return 2
-    if string_numbers == "3":
-        return 3
-    if string_numbers == "4":
-        return 4
-    if string_numbers == "5":
-        return 5
-    return 0
+    return int(string_numbers)
